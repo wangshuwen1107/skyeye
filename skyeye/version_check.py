b1735ba48bd06e14fa27f2e86629a6ca3e9e0e9d
wangshuwen15222 <wangshuwen222@hellobike.com>
2025-04-23 19:34:44 +0800

    release:3.7.0

--- skyeye/version_check.py
+++ skyeye/version_check.py
@@ -1,7 +1,10 @@
 # coding=UTF-8
 import requests
-from distutils.version import StrictVersion
 from skyeye.config import VERSION
+
+def version_tuple(v):
+    # 将版本号转换为可比较的元组 (处理简单数字版本)
+    return tuple(map(int, (v.split('.') + ['0', '0', '0'])[:3]))
 
 def checkVersions():
     url = "https://pypi.python.org/pypi/skyeye/json"
@@ -13,10 +16,10 @@ def checkVersions():
         releases = data["releases"]
         if(releases == None):
             return
-        versions = sorted(list(releases.keys()), key=StrictVersion, reverse=True)
+        versions = sorted(list(releases.keys()), key=version_tuple, reverse=True)
     except:
         pass
     if(versions and len(versions)>0):
-        hasNewVersion = StrictVersion(versions[0]) > StrictVersion(VERSION)
+        hasNewVersion = version_tuple(versions[0]) > version_tuple(VERSION)
         if hasNewVersion:
            print('🔥🔥🔥 版本升级提示，最新版本：'+versions[0]+" \n🔥🔥🔥请用该命令升级： pip3 install --upgrade skyeye")
